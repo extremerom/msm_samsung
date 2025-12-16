9c97169e32746b2a14f4eb233c25af79ef3db5e7
Rafael Alvarado Navarro <ealvaradobucio26@gmail.com>
2025-12-16 08:36:12 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,11 +5,9 @@ export WDIR="$(dirname $(readlink -f $0))" && cd "$WDIR"
 # Download and install Toolchain
 if [ ! -d "${WDIR}/kernel_platform/prebuilts" ]; then
     echo -e "[+] Downloading and installing Toolchain...\n"
-    sudo apt install rsync p7zip-full -y
-    curl -LO --progress-bar https://github.com/ravindu644/-android_kernel_sm_s931b/releases/download/toolchain/qcom-6.6-toolchain.tar.gz.zip
-    curl -LO --progress-bar https://github.com/ravindu644/-android_kernel_sm_s931b/releases/download/toolchain/qcom-6.6-toolchain.tar.gz.z01
-    7z x qcom-6.6-toolchain.tar.gz.zip && rm qcom-6.6-toolchain.tar.gz.zip qcom-6.6-toolchain.tar.gz.z01
-    tar -xvf qcom-6.6-toolchain.tar.gz && rm qcom-6.6-toolchain.tar.gz
+    sudo apt install rsync p7zip-full curl tar -y
+    curl -LO --progress-bar https://www.dropbox.com/scl/fi/mugzq17bjgonj9jqsvsq1/toolchain.tar.gz?rlkey=ebchnfs8pmezl92kzlg9clrmg&st=c9rh3z1v&dl=1
+    tar -xzf toolchain.tar.gz && rm toolchain.tar.gz
     chmod -R +x "${WDIR}/kernel_platform/prebuilts"    
 fi
 
@@ -31,36 +29,43 @@ fi
 
 #1. target config
 # pa1q_eur_open_user
-export MODEL="pa1q"
+export MODEL="dm2q"
 export PROJECT_NAME=${MODEL}
 export REGION="eur"
 export CARRIER="open"
 export TARGET_BUILD_VARIANT="user"
 		
 		
-#2. sun (sm8750) common config
-CHIPSET_NAME="sun"
+#2. kalama (sm8550) common config
+CHIPSET_NAME="kalama"
 
 export ANDROID_BUILD_TOP=${WDIR}
-export TARGET_PRODUCT=perf
+export TARGET_PRODUCT=gki
 export TARGET_BOARD_PLATFORM=gki
 
 export ANDROID_PRODUCT_OUT=${ANDROID_BUILD_TOP}/out/target/product/${MODEL}
 export OUT_DIR=${ANDROID_BUILD_TOP}/out/msm-${CHIPSET_NAME}-${CHIPSET_NAME}-${TARGET_PRODUCT}
 
 # for Lcd(techpack) driver build
-export KBUILD_EXTRA_SYMBOLS=${ANDROID_BUILD_TOP}/out/vendor/qcom/opensource/mmrm-driver/Module.symvers
+export KBUILD_EXTRA_SYMBOLS="${ANDROID_BUILD_TOP}/out/vendor/qcom/opensource/mmrm-driver/Module.symvers \
+		${ANDROID_BUILD_TOP}/out/vendor/qcom/opensource/mm-drivers/hw_fence/Module.symvers \
+		${ANDROID_BUILD_TOP}/out/vendor/qcom/opensource/mm-drivers/sync_fence/Module.symvers \
+		${ANDROID_BUILD_TOP}/out/vendor/qcom/opensource/mm-drivers/msm_ext_display/Module.symvers \
+		${ANDROID_BUILD_TOP}/out/vendor/qcom/opensource/securemsm-kernel/Module.symvers \
+"
 
 # for Audio(techpack) driver build
 export MODNAME=audio_dlkm
 
-export KBUILD_EXT_MODULES="\
-	../vendor/qcom/opensource/mmrm-driver \
-        ../vendor/qcom/opensource/mm-drivers/msm_ext_display \
-        ../vendor/qcom/opensource/mm-drivers/sync_fence \
-        ../vendor/qcom/opensource/mm-drivers/hw_fence \
-        ../vendor/qcom/opensource/securemsm-kernel \
-        "
+export KBUILD_EXT_MODULES="../vendor/qcom/opensource/mm-drivers/msm_ext_display \
+  ../vendor/qcom/opensource/mm-drivers/sync_fence \
+  ../vendor/qcom/opensource/mm-drivers/hw_fence \
+  ../vendor/qcom/opensource/mmrm-driver \
+  ../vendor/qcom/opensource/securemsm-kernel \
+  ../vendor/qcom/opensource/display-drivers/msm \
+  ../vendor/qcom/opensource/audio-kernel \
+  ../vendor/qcom/opensource/camera-kernel \
+  "
 
 # custom build options
 export GKI_BUILDSCRIPT="./build/android/prepare_vendor.sh"
